de91bd2702e5147f2c8759deee472fcb48e82b09
valej2020 <vj2020@nyu.edu>
2021-05-28 16:28:50 +0400

    testing for the button

---
+++ Testing/test_button_count/test_button_count.ino
@@ -0,0 +1,87 @@
+#include "M5Atom.h"
+
+void setup()
+{
+  Serial.begin(9600);
+  M5.begin(true, false, true);
+  delay(50);
+  M5.dis.drawpix(0, 0xf00000);
+}
+
+int counter = 0;
+
+
+
+
+void loop()
+{
+
+  if (M5.Btn.wasPressed())
+  {
+    counter++; //Increases the counter everytime the button is pressed
+  }
+
+  if (counter == 0) {
+    M5.dis.clear();
+    M5.dis.drawpix(0, 0x00f000);
+  } else if (counter == 1) {
+    while (counter == 1)
+
+    {
+      if (M5.Btn.wasPressed())
+      {
+        break; //Increases the counter everytime the button is pressed
+      }
+      M5.dis.drawpix(0, 0x00f000);
+      M5.dis.drawpix(1, 0x00f000);
+      M5.dis.drawpix(2, 0x00f000);
+      M5.dis.drawpix(3, 0x00f000);
+      M5.dis.drawpix(4, 0x00f000);
+      M5.dis.drawpix(5, 0x00f000);
+      M5.dis.drawpix(6, 0x00f000);
+      M5.dis.drawpix(7, 0x00f000);
+      M5.dis.drawpix(8, 0x00f000);
+      M5.dis.drawpix(9, 0x00f000);
+      M5.dis.drawpix(10, 0x00f000);
+      M5.dis.drawpix(11, 0x00f000);
+      M5.dis.drawpix(12, 0x00f000);
+      M5.dis.drawpix(13, 0x00f000);
+      M5.dis.drawpix(14, 0x00f000);
+      M5.dis.drawpix(15, 0x00f000);
+      M5.dis.drawpix(16, 0x00f000);
+      M5.dis.drawpix(17, 0x00f000);
+      M5.dis.drawpix(18, 0x00f000);
+      M5.dis.drawpix(19, 0x00f000);
+      M5.dis.drawpix(20, 0x00f000);
+      M5.dis.drawpix(21, 0x00f000);
+      M5.dis.drawpix(22, 0x00f000);
+      M5.dis.drawpix(23, 0x00f000);
+      M5.dis.drawpix(24, 0x00f000);
+      M5.dis.drawpix(25, 0x00f000);
+      delay(100);
+      M5.dis.clear();
+      delay(100);
+
+    }
+  }
+  else if (counter == 2) {
+    M5.dis.clear();
+    M5.dis.drawpix(2, 0x00f000);
+  }
+  else if (counter == 3) {
+    M5.dis.clear();
+    M5.dis.drawpix(3, 0x00f000);
+  }
+  else if (counter == 4) {
+    M5.dis.clear();
+    M5.dis.drawpix(4, 0x00f000);
+  }
+  else if (counter == 5) {
+    M5.dis.clear();
+    M5.dis.drawpix(5, 0x00f000);
+  }
+
+
+  delay(50);
+  M5.update();
+}
