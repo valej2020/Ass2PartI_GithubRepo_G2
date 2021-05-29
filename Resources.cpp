82ceb9b9f9b82fe22e7b459fc08ccb0927c0c347
ft2063 <72688128+ft2063@users.noreply.github.com>
2021-05-30 00:31:48 +0400

    Create Resources.cpp
    Some useful example to learn/inspiration!

---
+++ Resources.cpp
@@ -0,0 +1,10 @@
+//this is a trial 
+// this may help for acelerometer and temperature reading https://docs.m5stack.com/en/api/imu
+//easy example light https://scrapbox.io/jkoba/LED:_M5Stack_ATOM_Matrix
+//basics https://patriot-geek.blogspot.com/2020/05/atom-matrix-basic-display-usage.html
+//more examples https://patriot-geek.blogspot.com/2020/05/atom-matrix-onboard-ir-led-and-remote.html
+//TEMPERATURE SENSOR https://patriot-geek.blogspot.com/2020/05/atom-matrix-temperature-sensor.html
+//ACELERATION https://patriot-geek.blogspot.com/2020/05/atom-matrix-using-mpu6886-accelerometer.html
+
+
+
